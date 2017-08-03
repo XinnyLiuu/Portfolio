2bdc6ec5373a365d0ea5b0d4df430041db01b0f1
Xin Liu <xhliu97@gmail.com>
2017-08-03 17:17:56 -0400

    https redirect

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
 });
 
 // Routes for CSS, JS etc.
-app.use(express.static(path.join(__dirname, 'public'), { redirect: false }));
+app.use(express.static(path.join(__dirname, '/public'), { redirect: false }));
 
 // Express Config
 app.use(bodyParser.urlencoded({extended: true}));
