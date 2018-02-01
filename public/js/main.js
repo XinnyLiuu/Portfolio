675f7154e333b9174a22171cb21f80b21787c8d3
Xin Liu <xhliu97@gmail.com>
2018-02-01 10:48:28 -0500

    added figure captions per project item

--- public/js/main.js
+++ public/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
 		});
 });
 
-var slideIndex = 0;
+var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -50,18 +50,4 @@ function showDivs(n) {
         x[i].style.display = "none";
     }
     x[slideIndex-1].style.display = "block";
-}
-
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("item");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 5000); // Change image every 2 seconds
 }
