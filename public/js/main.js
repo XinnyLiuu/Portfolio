059351a8a29569c32260264f1344bf794e9d3ccb
Xin Liu <xhliu97@gmail.com>
2018-02-01 10:26:03 -0500

    removed bootstrap carousel, added manual next/prev to slideshow

--- public/js/main.js
+++ public/js/main.js
@@ -33,3 +33,35 @@ $(document).ready(function(){
 				}, 1500, 'swing');
 		});
 });
+
+var slideIndex = 0;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("item");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("item");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
